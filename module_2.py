cfe622d4894dee9fe7b334be8ac888e00307b3c7
Mikhail-Moskov <michailmoskow@gmail.com>
2024-11-22 17:42:33 +0900

    Add files via upload
    Практическое задание по теме "Переменные"

---
+++ module_2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+spend_hours = 1.5
+name_of_course = 'Python'
+time_for_homework = spend_hours / homework_count
+print (name_of_course + ',', 'Всего задач: ' + str(homework_count) + ',', 'затрачено часов: ' + str(spend_hours) + ',', 'среднее время выполнения: ' + str (time_for_homework) + ' часа.')
