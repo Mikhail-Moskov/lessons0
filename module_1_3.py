851a935c47269de18707e7af42208426739f449f
Mikhail-Moskov <michailmoskow@gmail.com>
2024-11-22 16:16:08 +0900

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Задание 1
+name = 'Михаил'
+print ('Name = ' + name)
+age = 35
+print ('Age = ' + str(age))
+age = age + 1
+print ('New age = ' + str(age))
+is_student = True
+print ('Is student = ' + str(is_student))
