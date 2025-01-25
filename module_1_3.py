6198fd29efce622d33b5e366489e6ef4551e5690
Mikhail-Moskov <michailmoskow@gmail.com>
2025-01-25 10:02:40 +0900

    Delete module_1_3.py

--- module_1_3.py
+++
@@ -1,9 +0,0 @@
-# Задание 1
-name = 'Михаил'
-print ('Name = ' + name)
-age = 35
-print ('Age = ' + str(age))
-age = age + 1
-print ('New age = ' + str(age))
-is_student = True
-print ('Is student = ' + str(is_student))
